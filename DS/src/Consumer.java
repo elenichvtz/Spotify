c3c6172b6b95dfc32f9db4886d865852b6778b45
unknown <elenich98@gmail.com>
2020-03-18 13:17:51 +0200

    intellij project

--- DS/src/Consumer.java
+++ DS/src/Consumer.java
@@ -1,13 +1,11 @@
-
 import java.io.*;
+
 //client
-public interface Consumer extends Nodessss {
+public interface Consumer extends Node {
 
     public abstract void register(Broker broker,ArtistName artist);
 
     public abstract void disconnect(Broker broker,ArtistName artist);
-///tgttgtgt
-//ffdfgfg
+
     public abstract void playData(ArtistName artist, Value val);
 }
-
---
+++ DS/src/Value.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+
+public class Value {
+
+    MusicFile musicfile;
+
+    Value(MusicFile musicFile){
+        this.musicfile = musicFile;
+
+    }
+
+    public void setMusicfile(MusicFile musicfile) {
+        this.musicfile = musicfile;
+    }
+
+    public MusicFile getMusicfile() {
+        return musicfile;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
