38f1844698dffb311f7865830279b91993cbe333
unknown <elenich98@gmail.com>
2020-03-18 13:23:26 +0200

    added src again

---
+++ src/ArtistName.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+
+
+public class ArtistName {
+    String artistName;
+
+    ArtistName(String artistName){
+        this.artistName = artistName;
+
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/PublisherNode.java
@@ -0,0 +1,21 @@
+public class PublisherNode extends NodeImpl implements Publisher {
+    @Override
+    public void getBrokerList() {
+        
+    }
+
+    @Override
+    public Broker hashTopic(ArtistName artist) {
+        return null;
+    }
+
+    @Override
+    public void push(ArtistName artist, Value val) {
+
+    }
+
+    @Override
+    public void notifyFailure(Broker broker) {
+
+    }
+}
