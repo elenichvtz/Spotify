8de72e25d4ea504588d801940ac4037a05b90415
unknown <elenich98@gmail.com>
2020-03-26 04:13:23 +0200

    still null pointer exception

--- map/src/Main.java
+++ map/src/Main.java
@@ -3,7 +3,6 @@ import com.mpatric.mp3agic.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,7 +10,6 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-
 
     char start = 'A';
     char end = 'M';
@@ -41,15 +39,11 @@ public class Main {
                             for (Path songs : currentsongs) {
                                 String foldercontents = path.concat("/").concat(file.getFileName().toString());
 
-                                //System.out.println(foldercontents+" giuhgfrhurfhjfrghfrg");
                                 if (!songs.getFileName().toString().startsWith(".")) {
                                     try {
-                                        //String songname = songs.getFileName().toString(); //return the name of the song in string
-                                        //System.out.println(songname);
+
                                         Mp3File mp3file = new Mp3File(songs);
-                                        System.out.println("575757");
-                                        //Mp3File mp3file = new Mp3File(foldercontents.concat("/").concat(songs.getFileName().toString()));
-                                        System.out.println(mp3file.getId3v2Tag().getTitle());
+                                        //System.out.println(mp3file.getId3v2Tag().getTitle());
 
                                         if (mp3file.hasId3v2Tag()) {
                                             System.out.println("YES FOR ID2");
@@ -57,7 +51,7 @@ public class Main {
                                             System.out.println("Name of artist with tag version 2 is " + id3v2Tag.getArtist());
 
                                             if (id3v2Tag.getArtist()!=null && (id3v2Tag.getArtist().charAt(0) >= this.start && id3v2Tag.getArtist().charAt(0) <= this.end)) {
-                                                System.out.println("2228328384756547");
+
                                                 System.out.println(id3v2Tag.getArtist());
                                                 System.out.println(artistMap.get(id3v2Tag.getArtist()));
                                                 if (!this.artistMap.containsKey(id3v2Tag.getArtist())) {
@@ -84,8 +78,7 @@ public class Main {
                                             ID3v1 id3v1Tag = mp3file.getId3v1Tag();
                                             System.out.println("YES");
 
-
-                                            if ((id3v1Tag.getArtist().charAt(0) >= this.start && id3v1Tag.getArtist().charAt(0) <= this.end) && !id3v1Tag.getArtist().isEmpty()) { //if artist already exists
+                                            if ((id3v1Tag.getArtist().charAt(0) >= this.start && id3v1Tag.getArtist().charAt(0) <= this.end) && id3v1Tag.getArtist()!=null) { //if artist already exists
                                                 System.out.println("YES");
                                                 if (this.artistMap.containsKey(id3v1Tag.getArtist())) {
                                                     // playlist.add(id3v1Tag.getTitle());
@@ -98,6 +91,13 @@ public class Main {
                                                     this.artistMap.put(id3v1Tag.getArtist(), playlist2);
                                                 }
 
+                                            }
+                                            else if(id3v1Tag.getArtist()==null && ('U'>= this.start && 'U'<=this.end)) {
+                                                ArrayList<String> playlist3 = new ArrayList<String>();
+                                                playlist3.add(id3v1Tag.getTitle());
+                                                id3v1Tag.setArtist("Unknown");
+                                                System.out.println(id3v1Tag.getArtist());
+                                                this.artistMap.put(id3v1Tag.getArtist(),playlist3);
                                             }
                                         }
                                     } catch (InvalidDataException | UnsupportedTagException e) {
@@ -115,7 +115,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	// write your code here
         Main m = new Main();
         m.init();
         Map<String , ArrayList<String>> k = m.getMap();
@@ -124,6 +123,5 @@ public class Main {
             String value = k.get(name).toString();
             System.out.println(name + " " + value);
         }
-
     }
 }
