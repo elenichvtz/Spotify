e49f84cdda9752a6fad6bbf2dbe7a253a339594a
emydn <emydan@hotmail.gr>
2020-04-11 17:30:21 +0300

    fixed artist doesn't exist

--- src/BrokerNode.java
+++ src/BrokerNode.java
@@ -5,21 +5,32 @@ import java.net.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//Server
-public class BrokerNode implements Broker{
+//Client & Server
+public class BrokerNode extends Thread implements Broker,Serializable {
 
     ServerSocket publisher_providerSocket;
-    Socket publisher_requestSocket = null;
+    Socket publisher_requestSocket;
     ServerSocket consumer_providerSocket;
-    Socket consumer_requestSocket = null;
+    Socket ppconnection;
     ObjectOutputStream out = null;
     ObjectInputStream in = null;
 
-    BigInteger key;
-    ArrayList<Publisher> publishers = new ArrayList<>();
+    ObjectOutputStream out2 = null;
+    ObjectInputStream in2 = null;
+
+    ObjectOutputStream out3 = null;
+    ObjectInputStream in3 = null;
+    ArtistName artistReceived= null;
+    Map<String, ArrayList<String>> mapreceived = new HashMap<String, ArrayList<String>>();
+    Map<Integer, Map<String, ArrayList<String>>> art2 = new HashMap<>();
+
+    static boolean p = false;
+    static boolean exist = false;
+
     String ip;
     int port;
 
@@ -29,127 +40,33 @@ public class BrokerNode implements Broker{
     }
 
     @Override
-    public void init() {
+    public synchronized void init() {
 
         try {
             this.publisher_providerSocket = new ServerSocket(this.port, 10);
-            //this.publisher_requestSocket = this.publisher_providerSocket.accept();
             System.out.println("broker provider socket connect");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
-            this.publisher_requestSocket = this.publisher_providerSocket.accept();
-            this.in = new ObjectInputStream(this.publisher_requestSocket.getInputStream());
-            //this.out = new ObjectOutputStream(this.publisher_requestSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //receive map, ip and port from publisher
-        try {
-            String publisherip = this.in.readUTF();
-            System.out.println(publisherip);
-            int publisherport = this.in.readInt();
-            System.out.println(publisherport);
-            char start = this.in.readChar();
-            char end = this.in.readChar();
-            System.out.println(start +" & "+ end);
-
-            PublisherNode pn = new PublisherNode(start, end, publisherip, publisherport);
-            registeredPublishers.add(pn);
-            System.out.println(registeredPublishers.isEmpty());
-
-            Object publishermap = this.in.readObject();
-            System.out.println(publishermap.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            this.consumer_providerSocket = new ServerSocket(this.port+2, 10);
-            //this.publisher_requestSocket = this.publisher_providerSocket.accept();
+            this.consumer_providerSocket = new ServerSocket(this.port + 1, 10);
             System.out.println("broker consumer provider socket connect");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        try {
-            this.consumer_requestSocket = this.consumer_providerSocket.accept();
-            this.in = new ObjectInputStream(this.consumer_requestSocket.getInputStream());
-            //this.out = new ObjectOutputStream(this.publisher_requestSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //receive ip and port from consumer
-        String consumerip = null;
-        try {
-            consumerip = this.in.readUTF();
-            System.out.println("con "+consumerip);
-            int consumerport = this.in.readInt();
-            System.out.println(consumerport);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //System.out.println(registeredPublishers.isEmpty());
-
-        this.key = calculateKeys();
-
-        try {
-            this.publisher_requestSocket = new Socket(this.ip, this.port+1);
-            this.out = new ObjectOutputStream(this.publisher_requestSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //send key to publisher
-        try {
-            this.out.writeObject(this.key);
-            this.out.flush();
-            System.out.println(this.key);
-            System.out.println("flush");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            this.consumer_requestSocket = new Socket(this.ip, this.port+1);
-            this.out = new ObjectOutputStream(this.consumer_requestSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //send key to consumer
-        try {
-            this.out.writeObject(this.key);
-            this.out.flush();
-            System.out.println(this.key);
-            System.out.println("flushed");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
-    public List<Broker> getBrokers() {
-        return null;
-    }
+    public BigInteger calculateKeys() {
 
-    @Override
-    public BigInteger calculateKeys(){
         String s = ip + port;
-
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] messageDigest = md.digest(s.getBytes());
+
+
             BigInteger no = new BigInteger(1, messageDigest);
 
             return no;
@@ -161,50 +78,21 @@ public class BrokerNode implements Broker{
     }
 
     @Override
-    public void connect(){
-        try {
-            this.publisher_requestSocket = this.publisher_providerSocket.accept();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void disconnect(){
-        try {
-            this.publisher_requestSocket.close();
-            this.publisher_providerSocket.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public Publisher acceptConnection(Publisher publisher){
-        try {
-            this.publisher_requestSocket = this.publisher_providerSocket.accept();
-            System.out.println("Connection accepted");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    public PublisherNode acceptConnection(PublisherNode publisher) {
+        registeredPublishers.add(publisher);
+        System.out.println("Connection accepted");
         return publisher;
     }
 
     @Override
-    public Consumer acceptConnection(Consumer consumer) {
-        try {
-            this.publisher_requestSocket = this.publisher_providerSocket.accept();
-            System.out.println("Connection accepted");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    public ConsumerNode acceptConnection(ConsumerNode consumer) {
+        registeredUsers.add(consumer);
+        System.out.println("Connection accepted");
         return consumer;
     }
 
     @Override
-    public void notifyPublisher(String name){
-        //Θα ενημερωνει ο broker τον καθε publisher για ποια κλειδια ειναι υπευθυνοι (για ποιο ευρος τιμων)
+    public void notifyPublisher(String name) {
 
         try {
             out = new ObjectOutputStream(this.publisher_requestSocket.getOutputStream());
@@ -217,16 +105,256 @@ public class BrokerNode implements Broker{
     }
 
     @Override
-    public void pull(ArtistName artist){
+    public void pull(ArtistName artist, String song) {
+
+        System.out.println("Inside pull....");
+        int publisherPort = 0;
+        for (Map.Entry<Integer,  Map<String, ArrayList<String>>> entry : art2.entrySet()) {
+
+            Map<String, ArrayList<String>> k = entry.getValue();
+            if (k.containsKey(artist.artistName)) {
+                publisherPort = entry.getKey();
+                break;
+            }
+        }
+        System.out.println("Correct publisher port is: "+publisherPort);
+
         try {
-            in = new ObjectInputStream(this.publisher_requestSocket.getInputStream());
+            ppconnection = new Socket("localhost",publisherPort+2);
+            System.out.println("Connected as client tot Publisher");
+            out3 = new ObjectOutputStream(ppconnection.getOutputStream());
+            System.out.println("yo");
+            in3 = new ObjectInputStream(ppconnection.getInputStream());
+            System.out.println("yo");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("it exists");
+        MusicFile f = new MusicFile(song, artist.getArtistName(), null, null, null, 0, 0);
+        Value value = new Value(f);
+        try {
+            this.out3.writeObject(artist);
+            this.out3.writeObject(value);
+            this.out3.flush();
+            int numOfchunks = in3.readInt();
+            out.writeInt(numOfchunks);
+            out.flush();
+            System.out.println("Chunks to receive from publisher are: "+numOfchunks);
+            try {
+                for (int i = 1; i <= numOfchunks; i++) {
+                    MusicFile m = (MusicFile) in3.readObject();
+
+                    System.out.println("Chunk id is: "+m.getChunkId());
+                    out.writeObject(m);
+                    out.flush();
+                    System.out.println("Ola good");
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String args[]){
-        BrokerNode b = new BrokerNode("127.0.0.1",4321);
+    public void setOut(ObjectOutputStream out){this.out = out;}
+
+    public void setIn(ObjectInputStream in) {this.in = in;}
+
+    public void setOut2(ObjectOutputStream out){this.out2 = out;}
+
+    public void setIn2(ObjectInputStream in) {this.in2 = in;}
+
+    public void setMapReceived(Map<String,ArrayList<String>> map){ this.mapreceived = map; }
+
+    public void setArt(Map<Integer, Map<String, ArrayList<String>>> art2) { this.art2 = art2; }
+
+    public Map getMapReceived() { return mapreceived; }
+
+    public void setArtistReceived(ArtistName artistReceived) { this.artistReceived = artistReceived; }
+
+    public ArtistName getArtistReceived() { return artistReceived; }
+
+    public ServerSocket getPublisherServerSocket() { return this.publisher_providerSocket; }
+
+    public ServerSocket getConsumerServerSocket() { return this.consumer_providerSocket; }
+
+    public int getBrokerPort() { return this.port; }
+
+    public static void main(String args[]) {
+
+        BrokerNode b = new BrokerNode("localhost", 7654);
+        BrokerNode b2 = new BrokerNode("localhost", 8765);
+        BrokerNode b3 = new BrokerNode("localhost", 9876);
         b.init();
+        b2.init();
+        b3.init();
+        brokers.add(b);
+
+        System.out.println(brokers.isEmpty());
+        brokers.add(b2);
+        brokers.add(b3);
+
+        Map<Integer, Map<String, ArrayList<String>>> art = new HashMap<>();
+
+        brokers.parallelStream().forEach((broker) -> {
+
+            if(broker.getBrokerPort()!=9876) {
+
+                try {
+
+                    Socket publisher = broker.getPublisherServerSocket().accept();
+                    broker.setOut2(new ObjectOutputStream(publisher.getOutputStream()));
+                    broker.setIn2(new ObjectInputStream(publisher.getInputStream()));
+
+                    //receive map, ip and port from publisher
+                    String publisherip = broker.in2.readUTF();
+                    System.out.println(publisherip);
+                    int publisherport = broker.in2.readInt();
+                    System.out.println(publisherport);
+                    char start = broker.in2.readChar();
+                    char end = broker.in2.readChar();
+                    System.out.println(start + " & " + end);
+                    System.out.println("Current broker port is: " + broker.getBrokerPort());
+
+                    broker.setMapReceived((Map<String, ArrayList<String>>) broker.in2.readObject());
+
+                    art.put(publisherport, broker.getMapReceived()); //add in art the maps received
+
+                    System.out.println(broker.getMapReceived().toString());
+
+                    PublisherNode pn = new PublisherNode(start, end, publisherip, publisherport);
+
+                    registeredPublishers.add(pn);
+
+                    System.out.println("Updating list with artist done");
+
+                    registeredPublishers.add(pn);
+
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            broker.setArt(art);
+
+            System.out.println("Is map empty?" + (broker.getMapReceived()).isEmpty());
+
+            while (true) {
+
+                try {
+                    // socket object to receive incoming consumer requests
+                    Socket consumer = broker.getConsumerServerSocket().accept();
+
+                    broker.setOut(new ObjectOutputStream(consumer.getOutputStream()));
+                    broker.setIn(new ObjectInputStream(consumer.getInputStream()));
+
+                    String consumerip = broker.in.readUTF();
+                    System.out.println("conumer ip is " + consumerip);
+                    int consumerport = broker.in.readInt();
+                    System.out.println("consumer port is "+consumerport);
+
+                    ConsumerNode cn = new ConsumerNode(consumerip, consumerport);
+
+                    registeredUsers.add(cn);
+
+                    System.out.println(registeredUsers.isEmpty());
+
+                    ArtistName artistName = null;
+                    try {
+                        artistName = (ArtistName) broker.in.readObject();
+                        System.out.println(artistName.toString() + " received from consumer");
+                        broker.setArtistReceived(artistName);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                    System.out.println("yep "+registeredPublishers.get(0).hashTopic(artistName).getBrokerPort());
+
+
+                    if(registeredPublishers.get(0).hashTopic(artistName).getBrokerPort() == broker.getBrokerPort()) {
+                        System.out.println("Yessss");
+
+                        if (broker.getMapReceived().containsKey(broker.getArtistReceived().getArtistName())) {
+                            System.out.println("it is exist");
+                        }     //den xreiazetai
+
+                        boolean f = false;
+
+
+                        for (Map.Entry<Integer,  Map<String, ArrayList<String>>> entry : art.entrySet()) {
+
+                            Map<String, ArrayList<String>> k = entry.getValue();
+
+                            for(Map.Entry<String, ArrayList<String>> entry2 : k.entrySet()) {
+
+                                if (entry2.getKey() != null) {
+
+                                    System.out.println("key is:" + entry2.getKey());
+                                    System.out.println(broker.getArtistReceived().getArtistName());
+                                    if (entry2.getKey().equals(broker.getArtistReceived().getArtistName()) && entry2.getKey() != null) {
+                                        f = true;
+                                        exist = true;
+                                        System.out.println("Yes it is equal");
+                                        System.out.println(entry2.getValue().toString());
+                                        List<String> songs = entry2.getValue();
+                                        System.out.println("P is" + broker.p);
+                                        if (!broker.p) {
+                                            broker.out.writeInt(broker.port);
+                                            System.out.println("Executed");
+                                        }
+
+                                        broker.p = false;
+
+                                        broker.out.writeInt(1);
+
+                                        broker.out.writeObject(songs); //στελνει την λιστα με τα τραγουδια του καλλιτεχνη που ζητησε
+                                        broker.out.flush();
+
+                                        String song = broker.in.readUTF();//δεχεται το τραγουδι που θελει ο Consumer
+
+                                        System.out.println("Song received : " + song);
+                                        System.out.println(art.size());
+
+                                        broker.pull(broker.getArtistReceived(), song);
+                                        break;
+                                    }
+                                }
+                                if (f) {
+                                    break;
+                                }
+                            }
+                        }
+                        if(!exist){
+                            System.out.println("Inside doesn't exist");
+                            broker.out.writeInt(0);
+                            broker.out.flush();
+
+                        }
+                    }
+                    else {
+                        System.out.println("Noooo "+registeredPublishers.get(0).hashTopic(artistName).getBrokerPort());
+
+                        System.out.println("yo");
+                        int port = registeredPublishers.get(0).hashTopic(artistName).getBrokerPort();
+                        broker.out.writeInt(port);
+                        broker.out.flush();
+                        System.out.println("yo");
+                        broker.out.close();
+                        broker.in.close();
+                        broker.p = true;
+                    }
+
+                    System.out.println("END");
+
+                } catch (IOException | NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
