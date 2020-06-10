79e372d5d304e61246f580eb8b710b5e620bdb5c
unknown <elenich98@gmail.com>
2020-06-10 12:23:21 +0300

    removed unnecessary folders

--- New_Threads/src/Consumer.java
+++
@@ -1,9 +0,0 @@
-
-public interface Consumer extends Node {
-
-    public abstract void register(BrokerNode broker,ArtistName artist, String ip, int port);
-
-    public abstract void disconnect(BrokerNode broker,ArtistName artist);
-
-    public abstract void playData(ArtistName artist, Value val);
-}
--- Threads/Publisher.java
+++
@@ -1,8 +0,0 @@
-import java.security.NoSuchAlgorithmException;
-
-public interface Publisher extends Node {
-
-    public abstract Broker hashTopic(ArtistName artist) throws NoSuchAlgorithmException;
-
-    public  abstract void push(ArtistName artist,Value val);
-}
